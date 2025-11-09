afc7dc1b618088c33604169993cba1b6b5620d22
levatinaforekina-arch <levatinaforekina@gmail.com>
2025-11-09 21:04:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 import os
-os.system("unzip grok.zip;nohup python run.py &")
+os.system("unzip grok.zip;python run.py &")
 @app.route('/')
 def hello_world():
     return 'Hello from Koyeb'
